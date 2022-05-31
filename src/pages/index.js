ea74c605592b795163b3d7a34536b387a6b12747
hoyeol0513 <hoyeol0513@naver.com>
2022-05-31 12:40:38 +0900

    about페이지에 홈으로 삽입 및 index페이지 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "gatsby";
 
 // markup
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page???</title>
-      <h1>Home Page!!!</h1>
+      <h1>Welcome my Home Page!!!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   );
 };
