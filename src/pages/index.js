bf60a95c09945201a9ad74b0c26f87cbcfc95482
hoyeol0513 <hoyeol0513@naver.com>
2022-06-02 06:56:37 +0900

    StaticImg 컴포넌트로 노출

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/layout";
 
@@ -7,6 +8,11 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <title>Home Page</title>
       <p>Welco me my Home Page!!!</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <hr />
     </Layout>
   );
 };
