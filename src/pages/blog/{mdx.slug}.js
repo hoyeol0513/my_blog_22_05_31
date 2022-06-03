b852aa9acff9e096dba66c59dfd712978b0f41e1
hoyeol0513 <hoyeol0513@naver.com>
2022-06-03 12:50:37 +0900

    블로그 태그 노출

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -7,22 +7,17 @@ import CodeBlock from "../../components/CodeBlock";
 import { MDXProvider } from "@mdx-js/react";
 
 const BlogPost = ({ data }) => {
-  /* //hero_image안에 뭐가 들어있는지 확인하기 위한 console
-  console.log(
-    `data.mdx.frontmatter.hero_image : ${data.mdx.frontmatter.hero_image}`
-  );
-  */
   /* 이미지 없는 글에 대한 처리 */
   //  data.mdx.frontmatter.hero_image 에 이미지가 null이면 false로 && 연산이 진행되지 않음.
   // 고로 && 오른쪽의 getImage(data.mdx.frontmatter.hero_image) 도 진행되지 않음.
   const image =
     data.mdx.frontmatter.hero_image &&
     getImage(data.mdx.frontmatter.hero_image);
+  const tags = data.mdx.frontmatter.tags;
 
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
-
       {/* 이미지 없는 글에 대한 처리 (위에 &&연산과 동일) */}
       {image && (
         // 안에서 두개 이상의 태그를 쓰려면 <></> 이 필요함. (한개는 필요없음.)
@@ -52,6 +47,8 @@ const BlogPost = ({ data }) => {
       >
         <MDXRenderer>{data.mdx.body}</MDXRenderer>
       </MDXProvider>
+      <hr />
+      TAGS : {tags && tags.join(", ")}
     </Layout>
   );
 };
@@ -69,6 +66,7 @@ export const query = graphql`
             gatsbyImageData
           }
         }
+        tags
       }
       body
     }
